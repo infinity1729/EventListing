0a8507a72afa9cd124ca694c7160948eb9ef4ec2
Deepak Verma <deepakverma.knp2019@gmail.com>
2022-12-26 15:37:31 +0530

    loginpage left  card

--- frontend/src/components/LoginPageCard/Card.jsx
+++ frontend/src/components/LoginPageCard/Card.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import styled from "styled-components";
+import "./card.css";
 
 function Card() {
   return (
     <>
       <Left>
-        <h1>Event Lister</h1>
-        <p>Hey There!</p>
-        <p>Welcome Back</p>
+        <div className="top_left">
+          <img src="https://event.iitg.ac.in/icann2019/Proceedings_LaTeX/2019/IITG_White.png" width="30" height="30" alt="logo" />
+          <p>Event Lister</p>
+        </div>
+        <h1>Hey There!</h1>
+        <div className="welcome">
+          <p>Welcome Back</p>
+          <p>You are just one step away to  your feed.</p>
+        </div>
         <p>Dont have an account ?</p>
-        <Button>Signup</Button>
+        <Button>Sign Up</Button>
       </Left>
       <Right>
         <h1>Sign In</h1>
-        <input type="text" placeholder="abc@gmail.com"></input>
+        <input type="email" placeholder="abc@gmail.com"></input>
         <input type="password" placeholder="password"></input>
         <Button>Sign In</Button>
       </Right>
@@ -24,15 +31,18 @@ function Card() {
 export default Card;
 
 const Left = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    border: 2px solid black;
-    padding: 5%;
-    border-radius: 10px 0 0 10px;
-    height: 40%;
-    box-shadow:-5px 0px 10px lightgrey;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  color: white;
+  padding: 2%;
+  height: 50%;
+  font-size: 13px;
+  font-weight: lighter;  
+  border-radius: 10px 0px 0px 10px;
+  box-shadow: 0 3px 10px rgb(0 0 0 / 0.5);
+  background-color: rgb(110, 99, 255);
 `
 
 
@@ -40,9 +50,10 @@ const Button = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    padding: 8% 20%;
+    padding: 5% 15%;
+    font-weight: 600;
     border-radius: 30px;
-    border: 2px solid black;
+    border: 2px solid white;
 `
 
 const Right = styled.div`
